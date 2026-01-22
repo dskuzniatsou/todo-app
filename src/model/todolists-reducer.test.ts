24f67dd50f11c0c7571789fdd4cc58d21ce50453
dskuzniatsou <sovyclub@gmail.com>
2026-01-23 00:00:50 +0300

    added actionCreator and test

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -1,7 +1,8 @@
 import uuid from "react-uuid";
 import type {Todo} from "../App.tsx";
 import { beforeEach, expect, test } from 'vitest'
-import {todolistsReducer} from "./todolists-reducer.ts";
+import {addTodoAC, deleteTodoAC, todolistsReducer, toggleTodoAC, updateTitleTodoAC} from "./todolists-reducer.ts";
+
 
 
 let todolistId1: string
@@ -12,21 +13,22 @@ beforeEach(() => {
     todolistId1 = uuid()
     todolistId2 = uuid()
 
+
     startState = [
         {id: todolistId1, text: 'What to learn', completed: false},
-        {id: todolistId2, text: 'What to buy', completed: true},
+        {id: todolistId2, text: 'What to buy', completed: true, tasks: []},
     ]
 })
 test ('correct todolist should be deleted', ()=>{
 
     //2.действие
-    const action = {
-        type: 'DELETE_TODO',
-        payload: {
-            id: todolistId1,
-        }
-    }
-        const endState = todolistsReducer(startState, action)
+    // const action = {
+    //     type: 'DELETE_TODO',
+    //     payload: {
+    //         id: todolistId1,
+    //     }
+    // } as const
+        const endState = todolistsReducer(startState, deleteTodoAC(todolistId1))
         // 3. Проверка, что действие измененило state соответствующим образом
         // в массиве останется один тудулист
         expect(endState.length).toBe(1)
@@ -34,3 +36,27 @@ test ('correct todolist should be deleted', ()=>{
         expect(endState[0].id).toBe(todolistId2)
 }
 )
+test ('correct todolist should be created', ()=>{
+
+
+    const endState = todolistsReducer(startState, addTodoAC( 'new todo'))
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].text).toBe('new todo')
+expect(endState[2].completed).toBe(false)
+})
+
+test ('correct todo should be toggled',()=> {
+    const completed = false
+    const endState = todolistsReducer(startState, toggleTodoAC(todolistId2, completed))
+
+    expect(endState[0].completed).toBe(false)
+    expect(endState[1].completed).toBe(false)
+})
+test ('correct todo should be toggled',()=> {
+    const text = 'Hello World!'
+    const endState = todolistsReducer(startState, updateTitleTodoAC(todolistId2, text))
+
+    expect(endState[0].text).toBe('What to learn')
+    expect(endState[1].text).toBe('Hello World!')
+})
--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -2,39 +2,50 @@ import type {Todo} from "../App.tsx";
 import uuid from "react-uuid";
 
 const initialState: Todo[] = []
-
+// заглушка
 export type TodolistsAction = {
     type: string
     payload: any
 }
+// создаем actionCreator
+export const deleteTodoAC = (id: string) => {
+    return {type: 'DELETE_TODO', payload: {id} as const}
+}
+export const addTodoAC = ( text: string) => {
+    return {type: 'ADD_TODO', payload: { text} as const}
+}
+export const toggleTodoAC = (id: string, completed: boolean) => {
+    return {type: 'TOGGLE_TODO', payload: {id,completed} as const}
+}
+export const updateTitleTodoAC = (id: string, text: string) => {
+    return {type: 'UPDATE_TODO_TITLE', payload: {id,text} as const}
+}
+
+export type DeleteTodoAction = ReturnType<typeof deleteTodoAC>
+export type AddTodoAction = ReturnType<typeof addTodoAC>
+export type ToggleTodoAction = ReturnType<typeof toggleTodoAC>
+export type UpdateTitleTodoAction = ReturnType<typeof updateTitleTodoAC>
+type Actions = DeleteTodoAction | AddTodoAction | ToggleTodoAction | UpdateTitleTodoAction
 
 export const todolistsReducer = (state: Todo[] = initialState, action: TodolistsAction): Todo[] => {
     switch (action.type) {
         case 'ADD_TODO': {
             const newTodo: Todo = {id: uuid(), text: action.payload.text, completed: false, tasks: []}// создать новую тему
-            return [...state, newTodo ]
+            return [...state, newTodo]
         }
-        case 'DELETE_TODO':{
-            return state.filter(todo => todo.id !== action.payload.id)}
-        case 'TOGGLE_TODO':
-            // setTodos(prev =>
-            //     prev.map(todo => {
-            //         if (todo.id !== todoId) return todo;
-            //
-            //         // ❗ если есть задачи — запрещаем ручное переключение
-            //         if (todo.tasks.length > 0) return todo;
-            //
-            //         return {
-            //             ...todo,
-            //             completed: !todo.completed,
-            //         };
-            //     })
-            // );
-
-            return state.map(todo=>todo.id === action.payload.id&&todo.tasks?.length? { ...todo, completed: !todo.completed }
+        case 'DELETE_TODO': {
+            return state.filter(todo => todo.id !== action.payload.id)
+        }
+        case 'TOGGLE_TODO': {
+            return state.map(todo => todo.id === action.payload.id && todo.tasks?.length ? {
+                    ...todo,
+                    completed: !todo.completed
+                }
                 : todo)
-        case 'UPDATE_TODO_TITLE':
-            return state
+        }
+        case 'UPDATE_TODO_TITLE': {
+            return state.map(todo=> todo.id===action.payload.id ? {...todo, text: action.payload.text}: todo)
+        }
         default:
             return state
     }
